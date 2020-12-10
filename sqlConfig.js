51e661a863f3086b64912e37d181a63afa87c75f
urbanseabear <calvin.rudy@gmail.com>
2020-12-10 16:29:47 -0500

    added pooling

--- sqlConfig.js
+++ sqlConfig.js
@@ -2,17 +2,22 @@
 const mysql = require('mysql');
 const Promise = require('bluebird');
 const database = 'CatwalkRatings';
-const connect = mysql.createConnection({
+const connect = mysql.createPool({
+  connectionLimit: 10,
   host: '3.15.209.217',
   user: 'root',
   password: 'BM0football',
+  database: 'CatwalkRatings',
   dialect: 'mysql'
 });
 
 const db = Promise.promisifyAll(connect, {multiArgs: true});
 
 db.connectAsync()
-  .then(() => db.queryAsync(`USE ${database}`));
+  .then(() => console.log('CONNECTION ESTABLISHED'))
+  .error(error => {
+    console.log('CONNECTION ERROR', error);
+  });
 
 module.exports = db;
 
